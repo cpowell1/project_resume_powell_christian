f900faf6848f1de62274ffc99a7966f7574b99d5
Christian Powell <christiant.powell@gmail.com>
2018-03-26 16:55:17 -0500

    Add progress bars to Skills section

--- js/main.js
+++ js/main.js
@@ -2,4 +2,39 @@ $(document).ready(function () {
     $('.tabs').tabslet();
 });
 
-
+$(document).ready(function () {
+    $('#skills').progressbar({
+        value: 90
+    });
+    $('#skills').css({
+        'background': '#ffffff',
+        'border': '1px solid #e3e3e3',
+        'color': '#ffffff',
+        'width': '20%',
+        'margin': '5%',
+    });
+});
+$(document).ready(function () {
+    $('#skills1').progressbar({
+        value: 70
+    });
+    $('#skills1').css({
+        'background': '#ffffff',
+        'border': '1px solid #e3e3e3',
+        'color': '#ffffff',
+        'width': '20%',
+        'margin': '5%',
+    });
+});
+$(document).ready(function () {
+    $('#skills2').progressbar({
+        value: 50
+    });
+    $('#skills2').css({
+        'background-color': '#ffffff',
+        'border': '1px solid #e3e3e3',
+        'color': '#ffffff',
+        'width': '20%',
+        'margin': '5%',
+    });
+});
