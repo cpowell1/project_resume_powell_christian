5e4015bf80b3a1251d93e56c932d397da4708140
Christian Powell <christiant.powell@gmail.com>
2018-03-26 18:41:48 -0500

    Add animated progress bar jQuery and CSS

--- js/main.js
+++ js/main.js
@@ -3,38 +3,9 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    $('#skills').progressbar({
-        value: 90
-    });
-    $('#skills').css({
-        'background': '#ffffff',
-        'border': '1px solid #e3e3e3',
-        'color': '#ffffff',
-        'width': '20%',
-        'margin': '5%',
-    });
-});
-$(document).ready(function () {
-    $('#skills1').progressbar({
-        value: 70
-    });
-    $('#skills1').css({
-        'background': '#ffffff',
-        'border': '1px solid #e3e3e3',
-        'color': '#ffffff',
-        'width': '20%',
-        'margin': '5%',
-    });
-});
-$(document).ready(function () {
-    $('#skills2').progressbar({
-        value: 50
-    });
-    $('#skills2').css({
-        'background-color': '#ffffff',
-        'border': '1px solid #e3e3e3',
-        'color': '#ffffff',
-        'width': '20%',
-        'margin': '5%',
+    jQuery('.skillbar').each(function () {
+        jQuery(this).find('.skillbar-bar').animate({
+            width: jQuery(this).attr('data-percent')
+        }, 3000);
     });
 });
